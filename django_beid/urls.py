77891fc89c1a321f190f539bd228ea42567bc6d6
fabien <fabien@serveur.elitic.be>
2014-02-28 12:57:38 +0100

    modified urls.py to go to admin by default

--- django_beid/urls.py
+++ django_beid/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_beid.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/admin/'), name='go-to-admin'),
 )
